e3a8dccabc443a5942dd81b13e83a939f6458e98
KH0ura1san <Gwh233@outlook.com>
2024-02-13 23:47:41 +0800

    Create Project

---
+++ index.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { init: initDB, Counter } = require("./db");
+
+const logger = morgan("tiny");
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(logger);
+
+// 首页
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+
+bootstrap();
