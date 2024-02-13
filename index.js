4de16bacc6da245a606f15da8c285ee3c055f424
KH0ura1san <Gwh233@outlook.com>
2024-02-13 23:58:18 +0800

    Create Project

--- index.js
+++ index.js
@@ -15,6 +15,28 @@ app.use(logger);
 // 首页
 app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+  const fs = require('fs')
+  const request = require('request')
+  return new Promise((resolve, reject) => {
+    request({
+      method: 'POST',
+      // url: 'http://api.weixin.qq.com/wxa/msg_sec_check?access_token=TOKEN',
+      url: 'http://api.weixin.qq.com/wxa/msg_sec_check', // 这里就是少了一个token
+      body: JSON.stringify({
+        "articles": [
+          {
+            "title": "Test",
+            "digest": "测试",
+            "content": "测试内容",
+          }
+        ]
+      })
+    },function (error, response) {
+      console.log('接口返回内容', response.body)
+      resolve(JSON.parse(response.body))
+    })
+  })
+
 });
 
 
